1bee23daa5f096ddb35cd08e76972e7e65d64a5b
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-08-01 20:21:34 +0200

    Make Movie Parcelable

--- app/src/main/java/cz/tomasfejfar/popularmovies/models/Movie.java
+++ app/src/main/java/cz/tomasfejfar/popularmovies/models/Movie.java
@@ -1,9 +1,14 @@
 package cz.tomasfejfar.popularmovies.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Movie {
+public class Movie implements Parcelable {
+
+
     String name;
 
     String image;
@@ -36,6 +41,29 @@ public class Movie {
         this.releaseDate = releaseDate;
         this.id = id;
     }
+
+    protected Movie(Parcel in) {
+        name = in.readString();
+        image = in.readString();
+        rating = in.readDouble();
+        popularity = in.readDouble()
+        backdrop = in.readString();
+        overview = in.readString();
+        releaseDate = in.readString();
+        id = in.readInt();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public String getName() {
         return name;
@@ -80,5 +108,21 @@ public class Movie {
                 item.getString("release_date"),
                 item.getInt("id")
         );
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(image);
+        dest.writeDouble(rating);
+        dest.writeDouble(popularity);
+        dest.writeString(backdrop);
+        dest.writeString(overview);
+        dest.writeString(releaseDate);
     }
 }
