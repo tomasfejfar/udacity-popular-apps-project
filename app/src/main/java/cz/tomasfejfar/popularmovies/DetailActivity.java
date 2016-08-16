74e08eaa8c5fddc5f888a2b9399432265ea3d072
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-08-16 16:58:24 +0200

    Detail is displayed

--- app/src/main/java/cz/tomasfejfar/popularmovies/DetailActivity.java
+++ app/src/main/java/cz/tomasfejfar/popularmovies/DetailActivity.java
@@ -1,11 +1,8 @@
 package cz.tomasfejfar.popularmovies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,15 +12,6 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- app/src/main/java/cz/tomasfejfar/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/cz/tomasfejfar/popularmovies/DetailActivityFragment.java
@@ -1,10 +1,16 @@
 package cz.tomasfejfar.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import cz.tomasfejfar.popularmovies.models.Movie;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,22 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Movie movie = getActivity().getIntent().getExtras().getParcelable(Movie.class.getCanonicalName());
+        View layout = inflater.inflate(R.layout.fragment_detail, container, false);
+        TextView name = (TextView) layout.findViewById(R.id.detail_name);
+        if (movie == null) {
+            throw new RuntimeException("Failed to pull parcelable from intent!");
+        }
+        name.setText(movie.getName());
+        ImageView image = (ImageView) layout.findViewById(R.id.detail_image);
+        Picasso.with(getContext())
+                .load("http://image.tmdb.org/t/p/w185/".concat(movie.getImage()))
+                .resize(185, 185)
+                .centerCrop()
+                .into(image);
+        TextView overview = (TextView) layout.findViewById(R.id.detail_overview);
+        overview.setText(movie.getOverview());
+        return layout;
     }
 }
