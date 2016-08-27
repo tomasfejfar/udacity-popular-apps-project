b666f3de102d74e92fe691d10b327f3793c95829
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-08-27 16:48:43 +0200

    Improving detail activity

--- app/src/main/java/cz/tomasfejfar/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/cz/tomasfejfar/popularmovies/DetailActivityFragment.java
@@ -1,5 +1,8 @@
 package cz.tomasfejfar.popularmovies;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,9 +11,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import cz.tomasfejfar.popularmovies.models.Movie;
 
@@ -37,21 +42,41 @@ public class DetailActivityFragment extends Fragment {
                 .build();
         Movie movie = getActivity().getIntent().getExtras().getParcelable(Movie.class.getCanonicalName());
         View layout = inflater.inflate(R.layout.fragment_detail, container, false);
-        TextView name = (TextView) layout.findViewById(R.id.detail_name);
         if (movie == null) {
             throw new RuntimeException("Failed to pull parcelable from intent!");
         }
+        TextView name = (TextView) layout.findViewById(R.id.detail_name);
         name.setText(movie.getName());
+        TextView rating = (TextView) layout.findViewById(R.id.detail_rating);
+        rating.setText(getContext().getString(R.string.detail_rating_default, movie.getRating()));
+        TextView releaseDate = (TextView) layout.findViewById(R.id.detail_release_date);
+        releaseDate.setText(getContext().getString(R.string.detail_release_date, movie.getReleaseDate()));
         ImageView image = (ImageView) layout.findViewById(R.id.detail_image);
         picasso.load("http://image.tmdb.org/t/p/w185/".concat(movie.getImage()))
                 .resize(185, 185)
                 .centerCrop()
                 .into(image);
-        ImageView headerImage = (ImageView) layout.findViewById(R.id.detail_header_image);
+        final LinearLayout mainLayout = (LinearLayout) layout.findViewById(R.id.detail_linear_layout);
         Log.d(LOG_TAG, "http://image.tmdb.org/t/p/w185/".concat(movie.getBackdrop()));
 
         picasso.load("http://image.tmdb.org/t/p/w185/".concat(movie.getBackdrop()))
-                .into(headerImage);
+                .into(new Target(){
+
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        mainLayout.setBackground(new BitmapDrawable(getContext().getResources(), bitmap));
+                    }
+
+                    @Override
+                    public void onBitmapFailed(final Drawable errorDrawable) {
+                        Log.d("TAG", "FAILED");
+                    }
+
+                    @Override
+                    public void onPrepareLoad(final Drawable placeHolderDrawable) {
+                        Log.d("TAG", "Prepare Load");
+                    }
+                });
         TextView overview = (TextView) layout.findViewById(R.id.detail_overview);
         overview.setText(movie.getOverview());
         return layout;
